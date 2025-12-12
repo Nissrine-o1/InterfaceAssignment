2fdd2080fe2ab23d769be044e9405f7510812a0c
Nissrine-o1 <nissrinealchkeiti@gmail.com>
2025-12-12 17:40:03 -0500

    Refactor Main method to include Employee and IQuittable
    Updated the Main method to create an Employee object and demonstrate polymorphism with IQuittable interface.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
-
 using System;
 
-namespace MethodAssignment
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Create an Employee object and assign properties
+        Employee employee = new Employee()
         {
-            MathOperations operations = new MathOperations();
-            operations.DoMath(5, 10);
-            operations.DoMath(num1: 7, num2: 3);
-            Console.ReadLine();
-        }
+            FirstName = "John",
+            LastName = "Doe",
+            Id = 101
+        };
+
+        // Polymorphism: Declare an object of type IQuittable
+        // Since Employee implements IQuittable, it can be assigned to this variable
+        IQuittable quittableEmployee = employee;
+
+        // Call the Quit method on the interface object
+        // The runtime knows to execute the Employee's Quit method
+        quittableEmployee.Quit();
+
+        // Pause the console so we can see the output
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 }
