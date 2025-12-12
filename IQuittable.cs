5caae4db415ae1e9600a19c14ce95777609e9e09
Nissrine-o1 <nissrinealchkeiti@gmail.com>
2025-12-12 17:43:30 -0500

    Add IQuittable interface with Quit method

---
+++ IQuittable.cs
@@ -0,0 +1,8 @@
+// Define an interface named IQuittable
+// Interfaces can be implemented by classes to ensure they provide specific methods
+public interface IQuittable
+{
+    // Define a method signature for Quit
+    // Classes that implement this interface must define this method
+    void Quit();
+}
