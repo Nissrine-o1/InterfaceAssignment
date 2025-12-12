61c886fcd908f686c9edafdfb1eb7e322fad2b92
Nissrine-o1 <nissrinealchkeiti@gmail.com>
2025-12-12 17:53:32 -0500

    Refactor Employee class and Quit method implementation
    Refactor Employee class to improve structure and clarity.

--- Employee.cs
+++ Employee.cs
@@ -1,17 +1,20 @@
-// Define a class called Employee
-// This class will inherit the IQuittable interface
-public class Employee : IQuittable
-{
-    // Properties of the Employee class
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int Id { get; set; }
+// Employee.cs
+using System;
 
-    // Implement the Quit method from IQuittable interface
-    // This method is called when the employee quits
-    public void Quit()
+namespace InterfaceAssignment
+{
+    // Employee class implements IQuittable interface
+    public class Employee : IQuittable
     {
-        // For this example, just print a message to the console
-        Console.WriteLine($"{FirstName} {LastName} (ID: {Id}) has quit the company.");
+        // Properties for employee's first and last name
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Implementation of Quit method from IQuittable interface
+        public void Quit()
+        {
+            // Write a message to the console indicating the employee has quit
+            Console.WriteLine($"{FirstName} {LastName} has quit the job.");
+        }
     }
 }
